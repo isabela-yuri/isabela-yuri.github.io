af99048e3738862356cb5a1aace0cbd1ff85fff4
yurilavinas <yclavinas@gmail.com>
2024-01-15 22:43:08 +0100

    Added small file

--- 6bc7410d1d5c571c49960a66fb96a54a856e3fb3/exec.R
+++ 6bc7410d1d5c571c49960a66fb96a54a856e3fb3/exec.R
@@ -9,8 +9,8 @@ knitr::opts_chunk$set()
 
 # Execute
 # render_site("fotos.Rmd")
-render_site("presentes.Rmd")
-render_site("rsvp.Rmd")
-render_site("index.Rmd")
-render_site("presenca.Rmd")
-# render_site()
+# render_site("presentes.Rmd")
+# render_site("rsvp.Rmd")
+# render_site("index.Rmd")
+# render_site("presenca.Rmd")
+render_site()
