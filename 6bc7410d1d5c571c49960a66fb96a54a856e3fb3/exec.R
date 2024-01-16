3e7687a895d7b700c6df108c5b859e247e97fc13
yurilavinas <yclavinas@gmail.com>
2024-01-16 16:58:31 +0100

    Added new photo test

--- 6bc7410d1d5c571c49960a66fb96a54a856e3fb3/exec.R
+++ 6bc7410d1d5c571c49960a66fb96a54a856e3fb3/exec.R
@@ -4,13 +4,14 @@ library(distill)
 library(dplyr)
 library(slickR)
 
+setwd("~/Documents/pessoal/casorio/isabela-yuri.github.io/6bc7410d1d5c571c49960a66fb96a54a856e3fb3/")
 
 knitr::opts_chunk$set()
 
 # Execute
-# render_site("fotos.Rmd")
+render_site("fotos_2.Rmd")
 # render_site("presentes.Rmd")
 # render_site("rsvp.Rmd")
 # render_site("index.Rmd")
 # render_site("presenca.Rmd")
-render_site()
+# render_site()
