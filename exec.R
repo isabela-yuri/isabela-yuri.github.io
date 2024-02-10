be945f31960516925cf139f5939c3be83c2363ca
yurilavinas <yclavinas@gmail.com>
2024-02-10 10:26:16 +0100

    02/08

---
+++ exec.R
@@ -0,0 +1,20 @@
+#Setup
+#Setup
+library(rmarkdown)
+library(distill)
+library(dplyr)
+library(slickR)
+library(htmltools)
+library(knitr)
+
+setwd("~/Documents/pessoal/casorio/isabela-yuri.github.io/")
+
+knitr::opts_chunk$set()
+
+# Execute
+# render_site("fotos_2.Rmd")
+# render_site("presentes.Rmd")
+# render_site("rsvp.Rmd")
+# render_site("index.Rmd")
+# render_site("presenca.Rmd")
+render_site()
