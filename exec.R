76202fb0d665d0b109fbc594617e40a5c30f9294
yurilavinas <yclavinas@gmail.com>
2024-02-11 13:55:07 +0100

    photo

--- exec.R
+++ exec.R
@@ -13,9 +13,9 @@ knitr::opts_chunk$set()
 
 # Execute
 # render_site("fotos_2.Rmd")
-render_site("presentes.Rmd")
+# render_site("presentes.Rmd")
 # render_site("rsvp.Rmd")
-# render_site("index.Rmd")
+render_site("index.Rmd")
 # render_site("travel.Rmd")
 # render_site("presenca.Rmd")
 # render_site()
